a20e9fb09a29869a711c06d622a18d5a9db20358
aakashkandasamy <aakashkandasamy@gmail.com>
2024-09-26 18:27:06 -0400

    Update Rotor.java
    new variable and starting with rotor function

--- Rotor.java
+++ Rotor.java
@@ -3,11 +3,16 @@ public class Rotor {
     
     private String rotorValues;
     private char startChar;
-        
+    private String initRot;
+    
     public Rotor(String v, char c){
         this.rotorValues = new String(v);
         this.startChar = c;
-        
+        this.initRot - new String (v);
+
+        while(this.rotorValues.charAt(0) != this.startChar)(
+            this.rotate();
+        )
         while(!this.rotate());
             
     }
