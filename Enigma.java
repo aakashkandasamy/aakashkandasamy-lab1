af6b9dda639cd4a60f1339f61f74679ff6a3578e
aakashkandasamy <aakashkandasamy@gmail.com>
2024-09-28 14:21:06 -0700

    debugging

--- Enigma.java
+++ Enigma.java
@@ -1,66 +1,60 @@
-public class Enigma{
+public class Enigma {
 
-    private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
+    private final String[] rotorConfigurations = {
+        "#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY",
         "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
-        "#TGOWHLIFMCSZYRVXQABUPEJKND"};
+        "#TGOWHLIFMCSZYRVXQABUPEJKND"
+    };
 
+    private Rotor[] rotors; 
 
-    private Rotor rotors[];
-        
-    public Enigma(int id1, int id2, int id3, String start){
+    public Enigma(int id1, int id2, int id3, String start) {
+        rotors = new Rotor[3]; 
 
-        rotors = new Rotor[3];
-        rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
-        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
-        rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));
-        
+        rotors[0] = new Rotor(rotorConfigurations[id1 - 1], start.charAt(0));  // Inner rotor
+        rotors[1] = new Rotor(rotorConfigurations[id2 - 1], start.charAt(1));  // Middle rotor
+        rotors[2] = new Rotor(rotorConfigurations[id3 - 1], start.charAt(2));  // Outer rotor
     }
 
-    public String encrypt(String message){
+    public String encrypt(String message) {
         StringBuilder result = new StringBuilder();
 
-        for(char c : message.toCharArray()){
+        for (char c : message.toCharArray()) {
             char firstPass = rotors[0].charAt(rotors[0].indexOf(c));
             char secondPass = rotors[1].charAt(rotors[1].indexOf(firstPass));
             char thirdPass = rotors[2].charAt(rotors[2].indexOf(secondPass));
 
             result.append(thirdPass);
-
             rotate();
         }
 
         return result.toString();
     }
 
-    public String decrypt(String message){        
+    public String decrypt(String message) {
         StringBuilder result = new StringBuilder();
 
-        for(char c : message.toCharArray()){
+        for (char c : message.toCharArray()) {
             int thirdPassIndex = rotors[2].indexOf(c);
             char secondPass = rotors[1].charAt(thirdPassIndex);
             int secondPassIndex = rotors[1].indexOf(secondPass);
             char firstPass = rotors[0].charAt(secondPassIndex);
-            int firstPassIndex = rotors[0].indexOf(firstPass);
 
-            result.append(rotors[0].charAt(firstPassIndex));
+            result.append(firstPass);
+
             rotate();
         }
+
         return result.toString();
     }
 
-
-    
-
-
-    
-    private void rotate(){
-        if(rotors[0].rotate()){
-            if(rotors[1].rotate()){
+    private void rotate() {
+        if (rotors[0].rotate()) {
+            if (rotors[1].rotate()) {
                 rotors[2].rotate();
             }
         }
     }
-    
 }
--- Rotor.java
+++ Rotor.java
@@ -1,32 +1,28 @@
 public class Rotor {
+    private String rotorValues;   
+    private final String initialValues; 
+    private final char startChar; 
 
-    
-    private String rotorValues;
-    private char startChar;
-    private String initRot;
-    
-    public Rotor(String v, char c){
-        this.rotorValues = new String(v);
-        this.startChar = c;
-        this.initRot = new String (v);
+    public Rotor(String rotorConfig, char startChar) {
+        this.rotorValues = rotorConfig;
+        this.initialValues = rotorConfig;
+        this.startChar = startChar;
 
-        while(this.rotorValues.charAt(0) != this.startChar){
-            this.rotate();
-        }            
-    }
-    
-    public boolean rotate(){
-        this.rotorValues = this.rotorValues.charAt(this.rotorValues.length() - 1) + this.rotorValues.substring(0, this.rotorValues.length() - 1);
-        return this.rotorValues.equals(this.initRot);
-    }
-    
-
-    public int indexOf(char c){
-        return this.rotorValues.indexOf(c);
+        while (rotorValues.charAt(0) != startChar) {
+            rotate();
+        }
     }
 
-    public char charAt(int idx){
-        return this.rotorValues.charAt(idx);
+    public boolean rotate() {
+        rotorValues = rotorValues.charAt(rotorValues.length() - 1) + rotorValues.substring(0, rotorValues.length() - 1);
+        return rotorValues.equals(initialValues);
+    }
+
+    public int indexOf(char c) {
+        return rotorValues.indexOf(c);
+    }
+
+    public char charAt(int index) {
+        return rotorValues.charAt(index);
     }
 }
-    
